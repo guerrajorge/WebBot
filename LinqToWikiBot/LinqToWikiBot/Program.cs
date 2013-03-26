69869dd697995ac5332e69922c2c58fff861496d
Jorge Guerra <jorgej.guerra@gmail.com>
2013-03-26 16:18:22 -0400

    WebBot code

---
+++ LinqToWikiBot/LinqToWikiBot/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Xml;
+using System.Xml.XPath;
+
+namespace LinqToWikiBot
+{
+    class Program
+    {
+        static StringBuilder subject = new StringBuilder();
+
+        static void Main(string[] args)
+        {
+
+            //Whatever wer are looking for
+            string subject = "";
+
+            Console.WriteLine("Input a subject");
+            subject = System.Console.ReadLine();
+
+            //This is where we are going to get the information
+            Uri address = getAddress(subject);
+
+            WebClient client = new WebClient();
+            printToFile(address, client);
+        }
+
+        private static void printToFile(Uri address, WebClient client)
+        {
+
+            client.Headers.Add("User-Agent", "TriviaGame");
+
+            try
+            {
+                string response = client.DownloadString(address);
+                formatSubjectString(response);
+
+                File.AppendAllText(@"C:\Users\Oer\Documents\Visual Studio 2012\Projects\LinqToWikiBot\wiki.xml", response);
+                Console.Write("Process finished...");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void formatSubjectString(string response)
+        {
+            StringBuilder sb = new StringBuilder();
+            StringBuilder sb2 = new StringBuilder();
+            sb2.Append(response);
+            sb2.Replace("\n", string.Empty);
+            sb2.Replace(" ", string.Empty);
+            response = sb2.ToString();
+            Boolean flag = false;
+
+            foreach (char c in response)
+            {
+                if (0 == c.CompareTo('['))
+                {
+                    if (flag)
+                    {
+                    }
+                    flag = true;
+                }
+            }
+            for (int i=0; i<response.Length;i++)
+            {
+                if (0 == response[i].CompareTo('['))
+                {
+                    if (0 == response[i + 1].CompareTo('['))
+                    {
+                        while(response[i] != ']')
+                        {
+                            i++;
+                            sb.Append(response[i]);
+                            
+                        }
+                        sb.AppendLine();
+                    }
+                }
+            }
+            Console.WriteLine("Done");
+        }
+
+        private static Uri getAddress(string subject)
+        {
+            //Variable definition
+            string action = "query";
+            string prop = "revisions";
+            string format = "txt";
+            string rvprop = "content";
+            int rvlimit = 10;
+            //int rvsection = 0;
+
+            //easiest to read info form
+            //Uri address = new Uri("http://en.wikipedia.org/w/api.php?action=query&prop=revisions&format=xml&rvprop=content&rvlimit=10&rvsection=0&titles=Country&titles=Country");
+
+            Uri address = new Uri("http://en.wikipedia.org/w/api.php?" + "action=" + action + "&prop=" + prop + "&format=" +
+                                            format + "&rvprop=" + rvprop + "&rvlimit=" + rvlimit + "&titles=" + subject + "&titles=" + subject);
+            return address;
+        }
+    }
+}
---
+++ LinqToWikiBot/LinqToWikiBot/SearchWord.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LinqToWikipedia;
+
+namespace LinqToWikiBot
+{
+    
+
+    class SearchWord
+    {
+        static WikipediaContext datacontext = new WikipediaContext();
+        static Dictionary<int, information_structure> dictionary = new Dictionary<int, information_structure>();
+        static information_structure info_struc;
+        
+        
+
+        private static void Write_Console()
+        {
+            foreach (KeyValuePair<int, information_structure> value in dictionary)
+            {
+                Console.WriteLine(value.Value.question + "?");
+                Console.WriteLine(value.Value.answer);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Process Finished...");
+            Console.ReadLine();
+        }
+
+        private static void Obtain_information()
+        {
+
+
+            /*
+             * Subjects
+             **/
+            String[] subject = new String[] { "Computer engineering", "Miami Heat", "Orlando Magic", 
+                "Venezuela", "Basketball", "Association football","Tennis", "Computer science", "Education", "Hacker" };
+            StringBuilder sb2 = new StringBuilder();
+
+
+            for (int i = 0; i < subject.Length; i++)
+            {
+
+                /*
+                 * creates object: opensearch, which uses:
+                 * subject = whatever it is being search in Wikipedia
+                 * take(1) = limits the search to one option (the first one and that is it!)
+                 * OpenSearch = the action in the wiki sandbox
+                 **/
+                var opensearch = (
+                from wikipedia in datacontext.OpenSearch
+                where wikipedia.Keyword == subject[i]
+                select wikipedia).Take(1);
+
+                info_struc.answer = subject[i];
+
+                foreach (WikipediaOpenSearchResult result in opensearch)
+                {
+                    /*
+                     * add the descriptions of the searched subject to the StringBuilder sb2 and replaces
+                     * the subject with ""(empty string") so the subject name is not shown in the question
+                     **/
+                    sb2.Append(result.Description).Replace(subject[i], "");
+                    /*
+                     * Make sure the string if formatted correctly and adds the interrogative pronous
+                     * where needed
+                     **/
+                    formatString(sb2);
+                    info_struc.question = sb2.ToString();
+                    //add the name and question to the dictionary list
+                    dictionary.Add(i, info_struc);
+                    //clears sb2 becuase if not then everytime we run dictionary.add ... whatever information is
+                    //in sb2 gets added to the second string, which we dont want DUH!
+                    sb2.Clear();
+                }
+            }
+        }
+
+        private static void formatString(StringBuilder sb2)
+        {
+            String info = sb2.ToString();
+            int index_is = 0;
+            int index_are = 0;
+            int point = 0;
+            StringBuilder sb3 = new StringBuilder();
+
+            //self explanatory, need to find the index
+            //so we can know where the string actually begin and remove
+            //any information before these verbs
+            index_is = info.IndexOf("is");
+            index_are = info.IndexOf("are");
+            point = info.LastIndexOf(".");
+
+            /*if any of these index are less than zero, it means that
+             * the IndexOf function did not find any string as "is" or "are"
+             * and therefore set them up to "-1". I then give a value of 200 so 
+             * either index becomes greater than the other one. 
+             * */
+            if (index_are < 0)
+                index_are = 200;
+            if (index_is < 0)
+                index_is = 200;
+
+            /*
+             * Now I can check if any of those tokens are in the string and 
+             * format the string correctly
+             **/
+            if (index_is > 0 && index_is < index_are)
+            {
+                sb2.Remove(0, index_is - 1);
+
+            }
+
+            else if (index_are > 0 && index_are < index_is)
+            {
+                sb2.Remove(0, index_are - 1);
+            }
+
+            //removes point in the description
+            if (sb2.Length != 0) { sb2.Length -= 2; }
+
+            /*
+             * Inserts the interrogative pronouns based on the category
+             **/
+            sb2.Insert(0, "What");
+
+        }
+
+    }
+
+    struct information_structure
+    {
+        public String category;
+        public String question;
+        public String answer;
+    };
+
+}
